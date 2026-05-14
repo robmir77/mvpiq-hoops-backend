5d3213891ed29205424f9da8d8c88b067a9877b4
Roberto Miranda <roberto.miranda@datamanagement.it>
2026-05-14 18:20:47 +0200

    avanzamento

--- src/main/java/com/mvpiq/model/JournalEntry.java
+++ src/main/java/com/mvpiq/model/JournalEntry.java
@@ -6,6 +6,8 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
 
 import java.time.OffsetDateTime;
 import java.util.HashSet;
@@ -63,6 +65,7 @@ public class JournalEntry {
     private Boolean checklistCompleted = false;
 
     @Column(name = "tags", columnDefinition = "jsonb")
+    @JdbcTypeCode(SqlTypes.JSON)
     private String tags;
 
     @Column(name = "deleted_at")
